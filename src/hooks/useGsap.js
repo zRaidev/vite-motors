b239ba4016a3473ef8aaed8cc08a815bb23b823b
macRaidev <isaac14e2007@gmail.com>
2026-01-05 02:31:36 -0500

    add useGsap hook for GSAP animations

---
+++ src/hooks/useGsap.js
@@ -0,0 +1,11 @@
+// hooks/useGsap.js
+import { useGSAP as gsapHook } from '@gsap/react'
+
+export const useGsap = (animationCallback, deps = [], scopeRef = null) => {
+  gsapHook(
+    () => {
+      animationCallback()
+    },
+    { dependencies: deps, scope: scopeRef }
+  )
+}
